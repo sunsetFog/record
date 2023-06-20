8b6e959a29037d88f60a4fccfafad9702d04e7ef
ivory <jsivory@itcom666.com>
2023-06-20 17:29:52 +0800

    2023-6-20 17:29

--- java/myspl/database/database02.sql
+++ java/myspl/database/database02.sql
@@ -1,9 +1,9 @@
 #练习 : database03.sql 备份hsp_db02 和 hsp_db03 库中的数据，并恢复
 
-#备份, 要在Dos下执行mysqldump指令其实在----mysql安装目录\bin
+#备份, 要在Dos下执行mysqldump指令其实在----mysql安装目录\bin    cd /D D:\java\mysql-5.7.19-winx64\bin
 #这个备份的文件，就是对应的sql语句
 # mysqldump -u 用户名 -p -B 数据库1 数据库2 数据库n > d:\\文件名.sql
-mysqldump -u root -p -B db_news > d:\\backups.sql
+mysqldump -u root -p -B db_news > d:\\java\backups.sql
 
 
 
